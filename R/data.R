d8b0bd8b5db9c00faa06a1e0300186dbd0e49cdf
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2023-04-18 14:02:19 -0700

    Polishing up 2022 data pdf, update help for data objects, add popeye and shortbelly rockfish to species names (https://github.com/pbs-assess/gfsynopsis/issues/144).

--- R/data.R
+++ R/data.R
@@ -230,32 +230,36 @@
 #'   for full details and how data were extracted from IPHC website.
 "setData2020"
 
-#' Count data for 2021 IPHC survey (following 2020 approach since 20-hooks again)
+#' Count data for 2021 and 2022 IPHC survey (following 2020 approach since 20-hooks again)
 #'
-#' A dataset containing counts of each species for each
-#' station for the 2021 IPHC survey.
+#' Datasets containing counts of each species for each
+#' station for the 2021 (`countData2021`) and 2022 (`countData2022`) IPHC surveys.
 #'
 #' @format A tibble:
 #' \describe{
 #'
-#'   \item{year}{2021 for this data set}
+#'   \item{year}{2021 or 2022}
 #'   \item{station}{station name}
 #'   \item{spNameIPHC}{species name for the counts for that set}
 #'   \item{specCount}{count of that species for that set}
 #' }
 #' @source See `data-raw/iphc-2021-data.Rmd` and saved
 #'   `data-raw/iphc-2021-data.pdf` for full details and how data were
-#'   extracted from IPHC website.
+#'   extracted from IPHC website, and similar for 2022.
 "countData2021"
 
-#' Station data for 2021 IPHC survey (following 2020 approach since 20-hooks again)
+#' @rdname countData2021
+"countData2022"
+
+#' Station data for 2021 and 2022 IPHC surveys (following 2020 approach since 20-hooks again)
 #'
-#' A dataset containing details of the stations for the 2021
-#' IPHC survey; one set was deployed at each station.
+#' A dataset containing details of the stations for the 2021 (`setData2021`) and
+#' 2022 (`setData2022`)
+#' IPHC surveys; one set was deployed at each station.
 #'
 #' @format A tibble:
 #' \describe{
-#'   \item{year}{2021 for this data set}
+#'   \item{year}{2021 or 2022}
 #'   \item{station}{station name}
 #'   \item{lat}{latitude of station}
 #'   \item{lon}{longitude of station}
@@ -273,10 +277,12 @@
 #'   calculate `E_it20`, but saving anyway)}
 #' }
 #' @source See `data-raw/iphc-2021-data.Rmd` and `data-raw/iphc-2021-data.pdf`
-#'   for full details and how data were extracted from IPHC website.
+#'   for full details and how data were extracted from IPHC website, and similar
+#'   files for 2022.
 "setData2021"
 
-
+#' @rdname setData2021
+"setData2022"
 
 #' Conversion from GFbio names to IPHC species names (for data not in GFbio)
 #'
